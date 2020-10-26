80bd18d02d3003adb32e050fdddd43ac1316ff79
Gajanana Ganjigatti <40160330+Gaju27@users.noreply.github.com>
2020-10-26 20:49:54 +0530

    Update test_image_convert.py

--- test_image_convert.py
+++ test_image_convert.py
@@ -24,55 +24,55 @@ README_CONTENT_CHECK_FOR = [
 
 ]
 
-path = 'C:/Users/gajanana_ganjigatti/Documents/Gaju_data/github/image_converter/image_store/'
+path = 'https://github.com/Gaju27/session11/tree/main/image_store/'
 
 
-#
-# def test_readme_exists():
-#     assert os.path.isfile("README.md"), "README.md file missing!"
-#
-#
-# def test_readme_contents():
-#     readme = open("README.md", "r")
-#     readme_words = readme.read().split()
-#     readme.close()
-#     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
-#
-#
-# def test_readme_proper_description():
-#     READMELOOKSGOOD = True
-#     f = open("README.md", "r")
-#     content = f.read()
-#     f.close()
-#     for c in README_CONTENT_CHECK_FOR:
-#         if c not in content:
-#             READMELOOKSGOOD = False
-#             pass
-#     assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
-#
-#
-# def test_readme_file_for_formatting():
-#     f = open("README.md", "r")
-#     content = f.read()
-#     f.close()
-#     assert content.count("#") >= 5
-#
-#
-# def test_indentations():
-#     for each_module in all_modules:
-#         lines = inspect.getsource(each_module)
-#         spaces = re.findall('/n +.', lines)
-#         for space in spaces:
-#             assert len(space) % 4 == 2, f"Your script {each_module} contains misplaced indentations"
-#             assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines"
-#
-#
-# def test_function_name_had_cap_letter():
-#     for each_module in all_modules:
-#         functions = inspect.getmembers(each_module, inspect.isfunction)
-#         for function in functions:
-#             assert len(
-#                 re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
+
+def test_readme_exists():
+    assert os.path.isfile("README.md"), "README.md file missing!"
+
+
+def test_readme_contents():
+    readme = open("README.md", "r")
+    readme_words = readme.read().split()
+    readme.close()
+    assert len(readme_words) >= 100, "Make your README.md file interesting! Add atleast 100 words"
+
+
+def test_readme_proper_description():
+    READMELOOKSGOOD = True
+    f = open("README.md", "r")
+    content = f.read()
+    f.close()
+    for c in README_CONTENT_CHECK_FOR:
+        if c not in content:
+            READMELOOKSGOOD = False
+            pass
+    assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
+
+
+def test_readme_file_for_formatting():
+    f = open("README.md", "r")
+    content = f.read()
+    f.close()
+    assert content.count("#") >= 5
+
+
+def test_indentations():
+    for each_module in all_modules:
+        lines = inspect.getsource(each_module)
+        spaces = re.findall('/n +.', lines)
+        for space in spaces:
+            assert len(space) % 4 == 2, f"Your script {each_module} contains misplaced indentations"
+            assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines"
+
+
+def test_function_name_had_cap_letter():
+    for each_module in all_modules:
+        functions = inspect.getmembers(each_module, inspect.isfunction)
+        for function in functions:
+            assert len(
+                re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
 
 
 def test_for_20_images():
@@ -163,7 +163,7 @@ def test_folder_resize_store_for_resize_to_500_height():
 def test_zip_center_crop_224x224():
     command = f'python image_con crp_px -file {path} -loc "center_crop_224x224" -wt 224 -ht 224'
     command_run = os.system(command)
-    assert command_run == 1, "center crop to 224x224"
+    assert command_run == 0, "center crop to 224x224"
 
 
 def test_folder_center_crop_224x224():
